71045facdf5c3959cba54f7a2954f08fd1921204
Sambhav Jain <sambhavjain2612@gmail.com>
2018-03-04 18:58:46 +0530

    add random word generation

--- startup_name_generator/lib/main.dart
+++ startup_name_generator/lib/main.dart
@@ -6,14 +6,32 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Startup Name Generator',
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text('Startup Name Generator'),
         ),
-        body: new Center(child: new Text('Startup Name Generator')),
+        body: new Center(
+          //child: new Text('Startup Name Generator'))
+          //child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
+        ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
