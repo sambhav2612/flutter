5f1b9318620fd121698bbe3606d5d0b880825bc2
Sambhav Jain <sambhavjain2612@gmail.com>
2018-03-04 18:30:27 +0530

    init startup name generator

---
+++ startup_name_generator/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Startup Name Generator',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Startup Name Generator'),
+        ),
+        body: new Center(child: new Text('Startup Name Generator')),
+      ),
+    );
+  }
+}
