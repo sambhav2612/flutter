db30b837bdbaa2886bf6d30f16b5d67cc527f49a
Sambhav Jain <sambhavjain2612@gmail.com>
2018-03-24 23:31:08 +0530

    small updates to main

--- chat/lib/main.dart
+++ chat/lib/main.dart
@@ -2,13 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    new MaterialApp(
-      title: "Chat",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Chat"),
-        ),
+    new ChatKaroBhai());
+}
+
+class ChatKaroBhai extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Chat Karo Bhai",
+      home: new ChatScreen()
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Chat Karo Bhai"),
       )
-    )
-  );
+    );
+  }
 }
