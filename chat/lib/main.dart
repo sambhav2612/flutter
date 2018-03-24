6d6d0093ff93028abfaf1527bdc704ea3994a1ed
Sambhav Jain <sambhavjain2612@gmail.com>
2018-03-25 01:03:01 +0530

    offline only final update

--- chat/lib/main.dart
+++ chat/lib/main.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 void main() {
   runApp(new ChatKaroBhai());
 }
 
 class ChatKaroBhai extends StatelessWidget {
+  final ThemeData kIOSTheme = new ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.grey[100],
+    primaryColorBrightness: Brightness.light,
+  );
+
+  final ThemeData kDefaultTheme = new ThemeData(
+    primarySwatch: Colors.purple,
+    accentColor: Colors.orangeAccent[400],
+  );
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(title: "Chat Karo Bhai", home: new ChatScreen());
+    return new MaterialApp(
+        title: "Chat Karo Bhai",
+        theme: defaultTargetPlatform == TargetPlatform.iOS
+            ? kIOSTheme
+            : kDefaultTheme,
+        home: new ChatScreen());
   }
 }
 
@@ -16,11 +33,34 @@ class ChatScreen extends StatefulWidget {
   State createState() => new ChatScreenState();
 }
 
-class ChatScreenState extends State<ChatScreen> {
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   final TextEditingController _textController = new TextEditingController();
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  bool _isComposing = false;
+
+  @override
+  void dispose() {
+    for (ChatMessage message in _messages)
+      message.animationController.dispose();
+    super.dispose();
+  }
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+    ChatMessage message = new ChatMessage(
+        text: text,
+        animationController: new AnimationController(
+          duration: new Duration(milliseconds: 400),
+          vsync: this,
+        ));
+    setState(() {
+      _messages.insert(0, message);
+    });
+
+    message.animationController.forward();
   }
 
   Widget _buildTextComposer() {
@@ -33,6 +73,11 @@ class ChatScreenState extends State<ChatScreen> {
                 new Flexible(
                   child: new TextField(
                     controller: _textController,
+                    onChanged: (String text) {
+                      setState(() {
+                        _isComposing = text.length >= 1;
+                      });
+                    },
                     onSubmitted: _handleSubmitted,
                     decoration: new InputDecoration.collapsed(
                         hintText: "Type your message"),
@@ -41,7 +86,9 @@ class ChatScreenState extends State<ChatScreen> {
                 new Container(
                   child: new IconButton(
                       icon: new Icon(Icons.send),
-                      onPressed: () => _handleSubmitted(_textController.text)),
+                      onPressed: () => _isComposing
+                          ? () => _handleSubmitted(_textController.text)
+                          : null),
                 )
               ],
             )));
@@ -50,10 +97,62 @@ class ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Chat Karo Bhai"),
-      ),
-      body: _buildTextComposer(),
-    );
+        appBar: new AppBar(
+          title: new Text("Chat Karo Bhai")
+        ),
+        body: new Column(
+          children: <Widget>[
+            new Flexible(
+              child: new ListView.builder(
+                  padding: new EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemBuilder: (_, int index) => _messages[index],
+                  itemCount: _messages.length),
+            ),
+            new Divider(height: 1.0),
+            new Container(
+              decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
+            )
+          ],
+        ));
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.animationController});
+  final String text;
+  final String _name = "Sambhav Jain";
+  final AnimationController animationController;
+
+  @override
+  Widget build(BuildContext context) {
+    return new SizeTransition(
+        sizeFactor: new CurvedAnimation(
+            parent: animationController, curve: Curves.easeInOut),
+        axisAlignment: 0.0,
+        child: new Container(
+          margin: const EdgeInsets.symmetric(vertical: 10.0),
+          child: new Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Container(
+                margin: const EdgeInsets.only(right: 16.0),
+                child: new CircleAvatar(child: new Text(_name[0])),
+              ),
+              new Expanded(
+                child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new Text(_name, style: Theme.of(context).textTheme.subhead),
+                    new Container(
+                        margin: const EdgeInsets.only(top: 5.0),
+                        child: new Text(text)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
