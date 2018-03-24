1508a0f05ea891399603e8a160faec2a26e6feec
Sambhav Jain <sambhavjain2612@gmail.com>
2018-03-24 23:50:25 +0530

    basic layout 101

--- chat/lib/main.dart
+++ chat/lib/main.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    new ChatKaroBhai());
+  runApp(new ChatKaroBhai());
 }
 
 class ChatKaroBhai extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: "Chat Karo Bhai",
-      home: new ChatScreen()
-    );
+    return new MaterialApp(title: "Chat Karo Bhai", home: new ChatScreen());
   }
 }
 
@@ -21,12 +17,43 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return new IconTheme(
+        data: new IconThemeData(color: Theme.of(context).accentColor),
+        child: new Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: new Row(
+              children: <Widget>[
+                new Flexible(
+                  child: new TextField(
+                    controller: _textController,
+                    onSubmitted: _handleSubmitted,
+                    decoration: new InputDecoration.collapsed(
+                        hintText: "Type your message"),
+                  ),
+                ),
+                new Container(
+                  child: new IconButton(
+                      icon: new Icon(Icons.send),
+                      onPressed: () => _handleSubmitted(_textController.text)),
+                )
+              ],
+            )));
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Chat Karo Bhai"),
-      )
+      ),
+      body: _buildTextComposer(),
     );
   }
 }
